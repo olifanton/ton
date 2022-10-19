0bc02d6d3b6592cd6328ff02a991798a67919af2
Roman Zaycev <box@romanzaycev.ru>
2022-10-19 23:04:39 +0700

    `GetBlockTransactions` integration test

---
+++ tests/Olifanton/Ton/IntegrationTests/ToncenterHttpClient/GetBlockTransactionsIntegrationTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Olifanton\Ton\IntegrationTests\ToncenterHttpClient;
+
+class GetBlockTransactionsIntegrationTest extends ToncenterHttpClientIntegrationTestCase
+{
+    /**
+     * @throws \Throwable
+     */
+    public function testSuccess(): void
+    {
+        $client = $this->getInstance();
+        $resp = $client->getBlockTransactions(
+            -1,
+            "-9223372036854775808",
+            4222449,
+        );
+
+        $this->assertEquals(-1, $resp->id->workchain);
+        $this->assertEquals(4222449, $resp->id->seqno);
+
+        $this->assertCount(3, $resp->transactions);
+    }
+}
